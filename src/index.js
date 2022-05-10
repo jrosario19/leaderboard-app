5a6385aab1d9a5161158a7ea04592961cc92167b
Juan Rosario <jrosario19@hotmail.com>
2022-05-10 12:00:18 -0400

    Update index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import './style.css';
-import {displayScores} from './modules/score.js'
+import { displayScores } from './modules/score.js';
 
 displayScores();
 
 window.onload = () => {
   displayScores();
-}
+};
